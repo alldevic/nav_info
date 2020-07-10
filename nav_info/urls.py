3c868c81156e5b0b2e3878029f82886ba91ce5f9
Nikolay Bely <belyynikolay@gmail.com>
2020-07-10 22:00:01 +0700

    Add simple swagger

--- nav_info/urls.py
+++ nav_info/urls.py
@@ -2,13 +2,33 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 admin.site.site_header = "Адмнистрирование Nav Info"
 admin.site.site_title = "Адмнистрирование Nav Info"
 admin.site.index_title = "Nav Info"
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Nav Info API",
+        default_version='v1',
+        contact=openapi.Contact(email="beliy_ns@kuzro.ru"),
+        license=openapi.License(name="MIT License"),
+    ),
+
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', schema_view.with_ui('swagger',
+                                     cache_timeout=0), name='docs-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
