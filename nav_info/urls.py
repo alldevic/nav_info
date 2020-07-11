ac375933c89664f33492612ba6cb8adc37777b68
Nikolay Bely <belyynikolay@gmail.com>
2020-07-12 01:02:17 +0700

    Add api versioning and developer info

--- nav_info/urls.py
+++ nav_info/urls.py
@@ -11,12 +11,18 @@ admin.site.site_header = "Адмнистрирование Nav Info"
 admin.site.site_title = "Адмнистрирование Nav Info"
 admin.site.index_title = "Nav Info"
 
+ver_tag = 'v1'
+prefix = f'api/{ver_tag}/'
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Nav Info API",
-        default_version='v1',
-        contact=openapi.Contact(email="beliy_ns@kuzro.ru"),
-        license=openapi.License(name="MIT License"),
+        default_version=ver_tag,
+        contact=openapi.Contact(name="Nikolay Bely",
+                                url="https://github.com/alldevic",
+                                email="beliy_ns@kuzro.ru"),
+        license=openapi.License(name="MIT License",
+                                url="https://github.com/alldevic/nav_info/blob/master/LICENSE"),
     ),
 
     public=True,
@@ -25,11 +31,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', schema_view.with_ui('swagger',
+    path(prefix, schema_view.with_ui('swagger',
                                      cache_timeout=0), name='docs-ui'),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include('djoser.urls')),
-    path('api/', include('soap_client.urls')),
+    path(f'{prefix}auth/', include('djoser.urls.authtoken')),
+    path(prefix, include('djoser.urls')),
+    path(prefix, include('soap_client.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
